05a33c9d11b097a0a32138183bba53601593ef25
longfei <353462633@qq.com>
2016-08-10 19:22:00 +0800

    播放器

---
+++ BigBirdie/Helper/MYMusicPlayer.h
@@ -0,0 +1,13 @@
+//
+//  MYMusicPlayer.h
+//  BigBirdie
+//
+//  Created by 郭龙飞 on 16/8/10.
+//  Copyright © 2016年 郭龙飞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MYMusicPlayer : NSObject
+
+@end
---
+++ BigBirdie/Helper/MYMusicPlayer.m
@@ -0,0 +1,13 @@
+//
+//  MYMusicPlayer.m
+//  BigBirdie
+//
+//  Created by 郭龙飞 on 16/8/10.
+//  Copyright © 2016年 郭龙飞. All rights reserved.
+//
+
+#import "MYMusicPlayer.h"
+
+@implementation MYMusicPlayer
+
+@end
