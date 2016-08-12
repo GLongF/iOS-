4026dd6d70a82565d0e6a67c2a0072fbd379c2a9
yanqingZhao <1305751865@qq.com>
2016-08-12 10:02:18 +0800

    添加播放器

--- BigBirdie/Helper/MYAudioPlayer.h
+++ BigBirdie/Helper/MYAudioPlayer.h
@@ -7,16 +7,47 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+@protocol MYAudioPlayerDelegate <NSObject>
+
+//定时器触发的刷新UI
+//参数：curTime当前播放时间（格式为xx:xx的字符串）totleTime总时间（格式为xx:xx的字符串）progress播放进度
+- (void)getCurTime:(NSString *)curTime totleTime:(NSString *)totleTime progress:(CGFloat)progress;
+
+//播放完成后的回调
+- (void)endOfPlayAction;
+
+//准备完成后的回调
+- (void)prepareEndCanPlay;
+
+//准备时 发生错误
+- (void)prepareFailedWithError:(NSError *)error;
+
+@end
+
 
 @interface MYAudioPlayer : NSObject
 
-@property (nonatomic, copy) NSString * audioUrl;
+//音频地址
+@property (nonatomic, copy)   NSString * audioUrl;
+//播放器
+@property (nonatomic, strong) AVPlayer * player;
+//播放资源
+@property (nonatomic, strong) AVPlayerItem * playerItem;
+
+@property (nonatomic, weak)   id<MYAudioPlayerDelegate> delegate;
+
 
 + (instancetype)shareInstance;
 
+//替换资源后，应该调用这个准备播放，在准备完成后，会有准备完成后的回调，然后再调用play方法进行播放
+- (void)prepareToPlay;
+//播放，在暂停后可以调用这个方法继续播放
 - (void)play;
+//暂停
 - (void)pause;
-- (void)stop;
-
+//跳转到某个进度播放
+- (void)seekToTimeWithProgress:(CGFloat)progress;
 
 @end
--- BigBirdie/Helper/MYAudioPlayer.m
+++ BigBirdie/Helper/MYAudioPlayer.m
@@ -7,12 +7,11 @@
 //
 
 #import "MYAudioPlayer.h"
-#import <AVFoundation/AVFoundation.h>
+
 
 @interface MYAudioPlayer ()
 
-@property (nonatomic, strong) AVPlayer * player;
-@property (nonatomic, strong) AVPlayerItem * playerItem;
+@property (nonatomic, strong)NSTimer * timer;
 
 @end
 
@@ -33,23 +32,136 @@
     self = [super init];
     if (self) {
         
+        self.player = [[AVPlayer alloc] init];
+        
+        //添加通知，捕获播放结束的状态
+        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(playEnd:) name:AVPlayerItemDidPlayToEndTimeNotification object:nil];
     }
     return self;
 }
 
-- (void)setAudioUrl:(NSString *)audioUrl {
+-(void) playEnd:(NSNotification *)sender
+{
+    //暂停计时器
+    [self pause];
+    [self.delegate endOfPlayAction];
+}
+
+- (void)prepareToPlay {
     
-    _audioUrl = [audioUrl copy];
-    NSURL * url = [NSURL URLWithString:audioUrl];
-    self.playerItem = [AVPlayerItem playerItemWithURL:url];
-    if (self.player == nil) {
-        //第一次创建播放器
+    //如果当前currentItem存在，先将其监听这移除
+    if (self.player.currentItem) {
         
-        self.player = [AVPlayer playerWithPlayerItem:self.playerItem];
-    }else {
-        //播放器已经创建 只需要替换资源
-        [self.player play];
+        [self.player.currentItem removeObserver:self forKeyPath:@"status"];
     }
+    
+    AVPlayerItem * item = [[ AVPlayerItem alloc] initWithURL:[NSURL URLWithString:self.audioUrl]];
+    [item addObserver:self forKeyPath:@"status" options:NSKeyValueObservingOptionNew context:nil];
+    [self.player replaceCurrentItemWithPlayerItem:item];
+}
+
+#pragma mark - KVO
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSString *,id> *)change context:(void *)context {
+    
+    if ([keyPath isEqualToString:@"status"]) {
+        
+        switch ([[change valueForKey:@"new"] integerValue]) {
+                
+            case AVPlayerItemStatusUnknown: {
+                
+                NSLog(@"不知道什么错误");
+                NSError * error;
+                [self.delegate prepareFailedWithError:error];
+            }
+                break;
+            case AVPlayerItemStatusReadyToPlay: {
+                
+                [self.delegate prepareEndCanPlay];
+            }
+                break;
+            case AVPlayerItemStatusFailed: {
+                
+                NSLog(@"准备失败");
+                NSError * error;
+                [self.delegate prepareFailedWithError:error];
+            }
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+- (void)play {
+    
+    //本来就在播放状态
+    if (!(self.timer == nil)) {
+        
+        return;
+    }
+    
+    self.timer = [NSTimer scheduledTimerWithTimeInterval:0.1f target:self selector:@selector(timerAction) userInfo:nil repeats:YES];
+    
+    [self.player play];
+}
+
+- (void)pause {
+    
+    [self.timer invalidate];
+    [self.player pause];
+}
+
+- (void)timerAction {
+    
+     [self.delegate getCurTime:[self valueToString:[self getCurTime]] totleTime:[self valueToString:[self getTotleTime]] progress:[self getProgress]];
+}
+
+- (void)seekToTimeWithProgress:(CGFloat)progress {
+    
+        // 先暂停
+    [self pause];
+        // 跳转
+    [self.player seekToTime:CMTimeMake(progress * [self getTotleTime], 1) completionHandler:^(BOOL finished) {
+            if (finished == YES) {
+                [self play];
+            }
+    }];
+}
+
+- (NSInteger)getCurTime {
+    
+    if (self.player.currentItem) {
+        
+        NSLog(@" %lld",self.player.currentTime.value / self.player.currentTime.timescale);
+        NSLog(@" %lld",self.player.currentItem.currentTime.value / self.player.currentItem.currentTime.timescale);
+        return self.player.currentTime.value / self.player.currentTime.timescale;
+    }
+    return 0;
+}
+
+// 获取总时长
+- (NSInteger)getTotleTime {
+    
+    CMTime totleTime = [self.player.currentItem duration];
+    if (totleTime.timescale == 0) {
+        
+        return 1;
+    }else {
+        
+        return totleTime.value /totleTime.timescale;
+    }
+}
+
+// 获取当前播放进度
+- (CGFloat)getProgress {
+    
+    return (CGFloat)[self getCurTime]/ (CGFloat)[self getTotleTime];
+}
+
+// 将整数秒转换为 00:00 格式的字符串
+- (NSString *)valueToString:(NSInteger)value {
+    
+    return [NSString stringWithFormat:@"%.2ld:%.2ld",value/60,value%60];
 }
 
 
